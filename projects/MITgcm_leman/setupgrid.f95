306c0f061b3be67f27ed2c4a27c04a18965397b9
Andrea <andrea.cimatoribus@gmail.com>
2017-03-10 14:57:31 +0100

    Working example on Leman grid

---
+++ projects/MITgcm_leman/setupgrid.f95
@@ -0,0 +1,110 @@
+SUBROUTINE setupgrid
+  
+  USE mod_getMITgcm
+  USE mod_param
+  USE mod_vel
+  
+  USE mod_time
+  USE mod_grid
+  USE mod_name
+  USE mod_vel
+
+  IMPLICIT none
+  ! =============================================================
+  !    ===  Set up the grid ===
+  ! =============================================================
+  ! Subroutine for defining the grid of MITgcm.
+  ! Run once before the loop starts.
+  ! -------------------------------------------------------------
+  ! The following arrays has to be populated:
+  !
+  !  dxdy - Horizontal area of cells (T points)
+  !  dz   - Thickness of standard level (T point) 
+  !  dzt  - Time-invariant thickness of level (T point)
+  !  dzu  - Time-invariant thickness of level (U point)
+  !  dzv  - Time-invariant thickness of level (V point)
+  !  kmt  - Number of levels from surface to seafloor (T point)
+  !  kmu  - Number of levels from surface to seafloor (U point)
+  !  kmv  - Number of levels from surface to seafloor (V point)
+  ! -------------------------------------------------------------
+
+
+  ! ===   ===   ===   ===   ===   ===   ===   ===   ===   ===   ===   ===
+  ! = Variables for filename generation 
+  INTEGER, SAVE                             :: nread,ndates
+  CHARACTER(LEN=10), SAVE                   :: fstamp
+  
+  ! = Loop variables
+  INTEGER                                   :: i ,j ,k
+  
+  ! = Variables used for getfield procedures
+  CHARACTER (len=200)                       :: gridfile
+  INTEGER                                   :: start1d, count1d
+  INTEGER, DIMENSION(2)                     :: start2d, count2d
+  INTEGER, DIMENSION(3)                     :: start3d, count3d
+
+  ! = MITgcm Grid fields
+  REAL, SAVE, ALLOCATABLE, DIMENSION(:,:,:) :: hFacW   ,hFacS, hFacC
+  
+  allocate ( hFacW(imt,jmt,km) ,hFacS(imt,jmt,km) )
+  allocate ( hFacC(imt,jmt,km)                    )
+  
+  ! ===   ===   ===   ===   ===   ===   ===   ===   ===   ===   ===   ===
+
+  ! ======================================================
+  !    ===  Set up the grid ===
+  ! ======================================================
+  ! Vertical beginning/end indices
+  start1d  = 1
+  count1d  = km
+  ! Horizontal beginning/end indices
+  start2d  = [   1,  1]
+  count2d  = [ imt,jmt]
+  ! 3D beginning/end indices
+  start3d  = [   1,  1, 1]
+  count3d  = [ imt,jmt,km]
+
+  ! load grid
+  gridfile = trim(inDataDir) // 'DRF.data'
+  dz       = get1dfield(gridfile, start1d, count1d)
+  dz       = dz(km:1:-1)
+  gridfile = trim(inDataDir) // 'RAC.data'
+  dxdy     = get2dfield(gridfile, start2d, count2d)
+  gridfile = trim(inDataDir) // 'DXG.data'
+  dxv(:,0:jmt-1) = get2dfield(gridfile, start2d, count2d)
+  gridfile = trim(inDataDir) // 'DYG.data'
+  dyu(0:imt-1,:) = get2dfield(gridfile, start2d, count2d) 
+  gridfile = trim(inDataDir) // 'hFacW.data'
+  hFacW    = get3dfield(gridfile, start3d, count3d)
+  gridfile = trim(inDataDir) // 'hFacS.data'
+  hFacS    = get3dfield(gridfile, start3d, count3d)
+  gridfile = trim(inDataDir) // 'hFacC.data'
+  hFacC    = get3dfield(gridfile, start3d, count3d)
+
+  kmt      = sum(ceiling(hFacC),3)
+  allocate ( kmu(imt,jmt), kmv(imt,jmt) )
+  kmu      = sum(ceiling(hFacW),3)
+  kmv      = sum(ceiling(hFacS),3)
+
+  allocate ( dzu(imt,jmt,km,1),dzv(imt,jmt,km,1) )
+
+  kloop: do k=1,km
+     dzt(:,:,km-k+1,1) = dz(k)*hFacC(:,:,k)
+     dzu(:,:,km-k+1,1) = dz(k)*hFacW(:,:,k)
+     dzv(:,:,km-k+1,1) = dz(k)*hFacS(:,:,k)
+  end do kloop
+  dzt(:,:,:,2) = dzt(:,:,:,1)
+
+  !
+  ! Ensure thickness is zero in invalid points
+  !
+  do k=1,km
+     where (k .le. (km-kmt(1:imt,1:jmt)))
+        dzt(:,:,k,1) = 0
+        dzu(:,:,k,1) = 0
+        dzv(:,:,k,1) = 0
+     end where
+  enddo 
+
+  return
+end SUBROUTINE setupgrid
